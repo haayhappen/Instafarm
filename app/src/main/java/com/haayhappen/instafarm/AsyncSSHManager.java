63b035c74aac6a75e65259fe2a60b0596825836b
Fynn Merlevede <fynn.merlevede@googlemail.com>
2017-04-27 11:48:29 +0200

    edit sshmanager to get response /in testing

--- app/src/main/java/com/haayhappen/instafarm/AsyncSSHManager.java
+++ app/src/main/java/com/haayhappen/instafarm/AsyncSSHManager.java
@@ -13,9 +13,12 @@ import com.jcraft.jsch.Session;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.Console;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -36,11 +39,11 @@ public class AsyncSSHManager extends AsyncTask<String, Void, String> {
         private static final String TAG = "SSH MANAGER";
         private AsyncResponse asyncResponse;
         private String response="";
-    private static Session session;
-    private static ChannelShell channel;
-    private static String username = "";
-    private static String password = "";
-    private static String hostname = "62.75.253.50";
+        private static Session session;
+        private static ChannelShell channel;
+        private static String username = "";
+        private static String password = "";
+        private static String hostname = "62.75.253.50";
 
         public interface AsyncResponse {
             void processFinish(String response);
@@ -57,7 +60,8 @@ public class AsyncSSHManager extends AsyncTask<String, Void, String> {
 
             //Do SSH Connection here
 
-            try {
+            //try {
+                /*
                 //response = executeRemoteCommand("root", "5Keosniluro", "62.75.253.50", 22);
 //                response = executeRemoteCommand("testuser", "testpw", hostname, 22);
                 JSch jsch = new JSch();
@@ -82,12 +86,86 @@ public class AsyncSSHManager extends AsyncTask<String, Void, String> {
                 channelssh.setCommand("cd Instagram-API-python/ ;touch hierlandetderuser.txt");
                 channelssh.connect();
                 channelssh.disconnect();
+*/
 
-            } catch (Exception e) {
-                    e.printStackTrace();
+                    StringBuffer result = new StringBuffer();
+
+                    Session session = null;
+                    ChannelExec channel = null;
+
+                    try {
+                        JSch jSch = new JSch();
+
+                        session = jSch.getSession("instafarm", "62.75.253.50", 22);
+                        session.connect();
+
+                        // Avoid asking for key confirmation
+                        Properties prop = new Properties();
+                        prop.put("StrictHostKeyChecking", "no");
+                        session.setConfig(prop);
+
+                        channel = (ChannelExec) session.openChannel("exec");
+                        channel.setCommand("cd Instagram-API-python/ ;python checkuser.py hackingismylifeanonymous passwort");
+                        channel.setInputStream(null);
+                        InputStream stdout = channel.getInputStream();
+                        InputStream stderr = channel.getErrStream();
+                        channel.connect();
+
+                        //waitForChannelClosed(channel);
+                        //Thread.sleep(10000L);
+
+                        if (channel.getExitStatus() != 0) {
+                            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stderr));
+                            readAll(bufferedReader, result);
+
+                            throw new Exception(result.toString());
+                        } else {
+                            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stdout));
+                            result = readAll(bufferedReader, result);
+                            response = String.valueOf(readAll(bufferedReader, result));
+                        }
+                    } catch (Exception e) {
+                        //throw new Exception(e);
+                        e.printStackTrace();
+                    } finally {
+                        if (channel != null) {
+                            channel.disconnect();
+                        }
+
+                        if (session != null) {
+                            session.disconnect();
+                        }
+                    }
+
+                    return result.toString();
+
+    }
+
+    private StringBuffer readAll(BufferedReader bufferedReader, StringBuffer result) {
+        try  {
+            long length = 0;
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                if (line.isEmpty()) {
+                    break;
                 }
+                length += line.length();
+            }
+            //System.out.println("Read length: " + length);
+            //System.out.println("Bufferd Reader output: "+line);
+            Log.d(TAG, "read length " + length);
+            Log.d(TAG, "buffered reader output " + line);
 
-        return response;
+            return new StringBuffer(line +result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+return new StringBuffer("Error handling shell output");
+    }
+
+    private void waitForChannelClosed(ChannelExec channel) {
+
     }
 
         @Override
